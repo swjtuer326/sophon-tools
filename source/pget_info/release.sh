90054b24a2344036d44febf892c64e60f8ebf19c
zetao.zhang <zetao.zhang@sophgo.com>
2024-09-21 14:17:16 +0800

    add zip for get_info

--- source/pget_info/release.sh
+++ source/pget_info/release.sh
@@ -2,13 +2,31 @@
 
 BUILD_RET=0
 
+export CMD_7Z=$(command -v 7z)
+export CMD_ZIP=$(command -v zip)
+
 echo "build get_info ..."
+
 
 rm -rf output
 
-mkdir output
+mkdir -p output/get_info
+cp get_info.sh output/get_info/
+cp get_info_log_to_png.* output/get_info/
 
-cp get_info.sh output/
-cp get_info_log_to_png.* output/
+pushd output
+	if [ -f "$CMD_7Z" ]; then
+		echo "found 7z"
+		$CMD_7Z a -mx9 get_info.zip get_info
+		BUILD_RET=$?
+	elif [ -f "$CMD_ZIP" ]; then
+		echo "found zip"
+		$CMD_ZIP -r -9 get_info.zip get_info
+		BUILD_RET=$?
+	else
+		echo "Unsatisfied build dependencies"
+		BUILD_RET=-1
+	fi
+popd
 
 exit $BUILD_RET
