c5e4d95429e2262fe1d26086c71c061f21affc9e
zetao.zhang <zetao.zhang@sophgo.com>
2024-12-10 16:01:57 +0800

    add memtest version at file

--- source/pmemtest/release.sh
+++ source/pmemtest/release.sh
@@ -5,23 +5,25 @@ BUILD_RET=0
 export CMD_7Z=$(command -v 7z)
 export CMD_ZIP=$(command -v zip)
 
-echo "build memtest_a53_gdma ..."
+VERSION=$(grep -r "MEMTEST VERSION:" memtest_a53_gdma/start.sh | awk '{print $(NF)}' | tr -d '"')
+
+echo "build memtest_a53_gdma ${VERSION} ..."
 
 
 rm -rf output
 
 mkdir -p output
-cp -r memtest_a53_gdma output
+cp -r memtest_a53_gdma output/memtest_a53_gdma_${VERSION}
 cp *.md output
 
 pushd output
 	if [ -f "$CMD_7Z" ]; then
 		echo "found 7z"
-		$CMD_7Z a -mx9 memtest_a53_gdma.zip memtest_a53_gdma
+		$CMD_7Z a -mx9 memtest_a53_gdma_${VERSION}.zip memtest_a53_gdma_${VERSION}
 		BUILD_RET=$?
 	elif [ -f "$CMD_ZIP" ]; then
 		echo "found zip"
-		$CMD_ZIP -r -9 memtest_a53_gdma.zip memtest_a53_gdma
+		$CMD_ZIP -r -9 memtest_a53_gdma_${VERSION}.zip memtest_a53_gdma_${VERSION}
 		BUILD_RET=$?
 	else
 		echo "Unsatisfied build dependencies"
